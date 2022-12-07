99666ddb6ab1680db99e871af795d9125b235426
debdeep.g <goswami.d@techsophy.com>
2022-12-07 12:27:46 +0530

    Added pattern matching

--- src/main/scala/Ninty99Problems/P05.scala
+++ src/main/scala/Ninty99Problems/P05.scala
@@ -1,5 +1,7 @@
 package Ninty99Problems
 
+import scala.annotation.tailrec
+
 object P05 extends App {
 
   //  Problem Statement
@@ -14,8 +16,10 @@ object P05 extends App {
   println(list)
   println(list.reverse)
   println(emptyList.reverse)
+
   //  Solution 2 with Recursion
   def reverse[T](list: List[T]): List[T] = {
+    @tailrec
     def inner(list: List[T], reversedList: List[T]): List[T] = {
       if (list.isEmpty) reversedList
       else inner(list.tail, list.head :: reversedList)
@@ -27,4 +31,20 @@ object P05 extends App {
   println(list)
   println(reverse(list))
   println(reverse(emptyList))
+
+  //  Solution 3 with Pattern Matching
+  def reverse_v2[T](list:List[T]): List[T] = {
+    @tailrec
+    def inner[T](list : List[T],reversedList:List[T]) : List[T] ={
+      list match {
+        case Nil => reversedList
+        case h :: tail => inner(list.tail,h::reversedList)
+      }
+    }
+    inner(list,List())
+  }
+
+  println(list)
+  println(reverse_v2(list))
+  println(reverse_v2(emptyList))
 }
