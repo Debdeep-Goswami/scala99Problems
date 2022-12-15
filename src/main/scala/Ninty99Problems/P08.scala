1bf37b153d7541d78da84cebea03a07c62d1f245
debdeep.g <goswami.d@techsophy.com>
2022-12-15 15:23:44 +0530

    Updated the solution

--- src/main/scala/Ninty99Problems/P08.scala
+++ src/main/scala/Ninty99Problems/P08.scala
@@ -12,37 +12,36 @@ object P08 extends App {
   val list: List[Int] = List(1, 2, 2, 3, 4, 4, 5, 5, 6, 6, 6, 7)
   val list2: List[Char] = List('a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e')
 
-  def dropDuplicate[T](list: List[T]): List[T] = {
-    @tailrec
-    def inner[T](list: List[T], ans: List[T]): List[T] = {
-      list match {
-        case h :: tail => inner(tail.dropWhile(_ == h), h :: ans)
-        case nil => ans.reverse
+    def dropDuplicate[T](list: List[T]): List[T] = {
+      @tailrec
+      def inner[T](list: List[T], ans: List[T]): List[T] = {
+        list match {
+          case h :: tail => inner(tail.dropWhile(_ == h), h :: ans)
+          case nil => ans.reverse
+        }
+      }
+
+      inner(list, List.empty[T])
+    }
+
+    println(dropDuplicate(list))
+    println(dropDuplicate(list2))
+
+  def removeDuplicate[T](list: List[T]): List[T] = {
+    def inner[T](list: List[T], prev: Option[T], ans: List[T]): List[T] = {
+      if (list.isEmpty) ans.reverse
+      else if (prev == None) inner(list, Some(list.head), list.head :: ans)
+      else {
+        list match {
+          case x :: tail => if (x == prev.get) inner(tail, prev, ans) else inner(tail, Some(x), x :: ans)
+        }
       }
     }
 
-    inner(list, List.empty[T])
+    inner(list, None, List.empty[T])
   }
 
-  println(dropDuplicate(list))
-  println(dropDuplicate(list2))
-
-  //  Using fold function
-  println(
-    list.foldRight(List[Int]())((h, r) =>
-      if (r.isEmpty || r.head != h) h :: r
-      else r
-    )
-  )
-
-  //  Using Generics with fold
-  def dropDuplicate_v2[T](list: List[T]): List[T] = {
-    list.foldRight(List[T]())((h, r) =>
-      if (r.isEmpty || r.head != h) h :: r
-      else r
-    )
-  }
-
-  println(dropDuplicate_v2(list))
-  println(dropDuplicate_v2(list2))
+  println(removeDuplicate(list))
+  println(removeDuplicate(list2))
+  println(removeDuplicate(List.empty[Int]))
 }
