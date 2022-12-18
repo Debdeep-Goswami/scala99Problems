9705b70c2834016e739048721e5bfbfa543f4584
debdeep.g <goswami.d@techsophy.com>
2022-12-19 01:34:52 +0530

    Added Binary Search and Longest Sequence

--- src/main/scala/MixedProblems/BinarySearch.scala
+++ src/main/scala/MixedProblems/BinarySearch.scala
@@ -1,8 +1,12 @@
 package MixedProblems
 
+import scala.annotation.tailrec
+
 object BinarySearch extends App {
 
+  //  With List
   def divideListByMid(list: List[Int]): (Option[Int], List[Int], List[Int]) = {
+    @tailrec
     def inner(list: List[Int], start: Int, size: Int, leftPart: List[Int], rightPart: List[Int]): (Option[Int], List[Int], List[Int]) = {
       if (list.size == 0) (None, leftPart, rightPart)
       else if (start == size / 2) (Some(list.head), leftPart, rightPart)
@@ -13,6 +17,7 @@ object BinarySearch extends App {
   }
 
   def binarySearch(list: List[Int], searchElem: Int): Boolean = {
+    @tailrec
     def inner(list: List[Int], searchElem: Int, ans: Boolean): Boolean = {
       val (mid, leftHalf, rightHalf) = divideListByMid(list)
       if (list.isEmpty) ans
@@ -26,4 +31,93 @@ object BinarySearch extends App {
 
   val list: List[Int] = List(1, 2, 3, 4, 5, 6, 7, 8, 9)
 
-  println(bin
+  //  Element not present
+  var searchElement = 10
+  println(list + " Searched Element " + searchElement + " = " + binarySearch(list, searchElement))
+
+  searchElement = 0
+  println(list + " Searched Element " + searchElement + " = " + binarySearch(list, searchElement))
+
+
+  //  Element present
+  searchElement = 4
+  println(list + " Searched Element " + searchElement + " = " + binarySearch(list, searchElement))
+
+  //  Edge cases
+  searchElement = 1
+  println(list + " Searched Element " + searchElement + " = " + binarySearch(list, searchElement))
+
+  searchElement = 9
+  println(list + " Searched Element " + searchElement + " = " + binarySearch(list, searchElement))
+
+
+  //  Empty List
+  val emptyList = List.empty[Int]
+  searchElement = 100
+  println(emptyList + " Searched Element " + searchElement + " = " + binarySearch(emptyList, searchElement))
+
+  //  One Element List
+  val list1: List[Int] = List(5)
+  //  Element not present
+  searchElement = 100
+  println(list1 + " Searched Element " + searchElement + " = " + binarySearch(list1, searchElement))
+
+  //  Element present
+  searchElement = 5
+  println(list1 + " Searched Element " + searchElement + " = " + binarySearch(list1, searchElement))
+
+
+  //  _____________________________________________________
+
+  //  With Array
+  def binarySearch_v2(array: Array[Int], searchElem: Int): Boolean = {
+    @tailrec
+    def inner(array: Array[Int], searchElem: Int, ans: Boolean): Boolean = {
+      val mid = array.length / 2
+      if (array.isEmpty) ans
+      else if (array(mid) == searchElem) true
+      else if (searchElem > array(mid)) inner(array.slice(mid + 1, array.size), searchElem, ans)
+      else inner(array.slice(0, mid), searchElem, ans)
+    }
+
+    inner(array, searchElem, false)
+  }
+
+  val array: Array[Int] = Array(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+  //  Element not present
+  searchElement = 10
+  println(array.mkString(" ") + " Searched Element " + searchElement + " = " + binarySearch_v2(array, searchElement))
+
+  searchElement = 0
+  println(array.mkString(" ") + " Searched Element " + searchElement + " = " + binarySearch_v2(array, searchElement))
+
+
+  //  Element present
+  searchElement = 4
+  println(array.mkString(" ") + " Searched Element " + searchElement + " = " + binarySearch_v2(array, searchElement))
+
+  //  Edge cases
+  searchElement = 1
+  println(array.mkString(" ") + " Searched Element " + searchElement + " = " + binarySearch_v2(array, searchElement))
+
+  searchElement = 9
+  println(array.mkString(" ") + " Searched Element " + searchElement + " = " + binarySearch_v2(array, searchElement))
+
+
+  //  Empty Array
+  val emptyArray = Array.empty[Int]
+  searchElement = 100
+  println(emptyArray.toList + " Searched Element " + searchElement + " = " + binarySearch_v2(emptyArray, searchElement))
+
+  //  One Element Array
+  val array1: Array[Int] = Array(5)
+  //  Element not present
+  searchElement = 100
+  println(array1.mkString(" ") + " Searched Element " + searchElement + " = " + binarySearch_v2(array1, searchElement))
+
+  //  Element present
+  searchElement = 5
+  println(array1.mkString(" ") + " Searched Element " + searchElement + " = " + binarySearch_v2(array1, searchElement))
+
+}
---
+++ src/main/scala/MixedProblems/LongestSequence.scala
@@ -0,0 +1,72 @@
+package MixedProblems
+
+import scala.annotation.tailrec
+
+object LongestSequence extends App {
+
+  case class LongestSequenceInfo(element: Option[Int], frequence: Option[Int])
+
+  //  My way
+  def longestConsecutiveSequence(list: List[Int]): LongestSequenceInfo = {
+    @tailrec
+    def inner(list: List[Int], currentElement: Option[Int], currentFrequency: Option[Int], maxElement: Option[Int], maxFrequency: Option[Int]): LongestSequenceInfo = {
+      if (list.isEmpty) LongestSequenceInfo(maxElement, maxFrequency)
+      else {
+        val element = list.head
+        if (currentElement == None) inner(list.tail, Some(element), Some(1), Some(element), Some(1))
+        else {
+          if (element == currentElement.get) {
+            if (currentFrequency.get + 1 > maxFrequency.get) inner(list.tail, currentElement, Some(currentFrequency.get + 1), currentElement, Some(currentFrequency.get + 1))
+            else inner(list.tail, currentElement, Some(currentFrequency.get + 1), maxElement, maxFrequency)
+          }
+          else inner(list.tail, Some(element), Some(1), maxElement, maxFrequency)
+        }
+      }
+    }
+
+    inner(list, None, None, None, None)
+  }
+
+  val list: List[Int] = List(1, 2, 2, 3, 3, 3, 3, 4, 4, 5)
+  val ans: LongestSequenceInfo = longestConsecutiveSequence(list)
+  println("\nElement = " + ans.element + "\nFrequency = " + ans.frequence)
+
+
+
+  //  Updated with for comprehension
+  def longestConsecutiveSequence_v2(list: List[Int]): LongestSequenceInfo = {
+    def inner(list: List[Int], currentElement: Option[Int], currentFrequency: Option[Int], maxElement: Option[Int], maxFrequency: Option[Int]): LongestSequenceInfo = {
+      if (list.isEmpty) LongestSequenceInfo(maxElement, maxFrequency)
+      else {
+        val element = list.head
+        if (currentElement == None) inner(list.tail, Some(element), Some(1), Some(element), Some(1))
+        else {
+          if (currentElement.contains(element)) {
+            val result = for {
+              freq <- currentFrequency
+              maxFreq <- maxFrequency
+            } yield {
+              if (freq + 1 > maxFreq) inner(list.tail, currentElement, currentFrequency.map(_ + 1), currentElement, Some(freq + 1))
+              else inner(list.tail, currentElement, Some(freq + 1), maxElement, maxFrequency)
+            }
+            result.get
+          } else {
+            inner(list.tail, Some(element), Some(1), maxElement, maxFrequency)
+          }
+        }
+      }
+    }
+
+    inner(list, None, None, None, None)
+  }
+
+  val ans2: LongestSequenceInfo = longestConsecutiveSequence_v2(list)
+  println("\nElement = " + ans2.element + "\nFrequency = " + ans2.frequence)
+
+
+  //  Using Higher Order Function
+
+  val temp = list.groupBy(x => x).maxBy(x => x._2.size)
+  val ans3 = LongestSequenceInfo(Some(temp._1), Some(temp._2.size))
+  println("\nElement = " + ans3.element + "\nFrequency = " + ans3.frequence)
+}
